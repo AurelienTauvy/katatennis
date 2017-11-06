90bac295b37cb00bb7aa7f528549f3fb6c80420b
tauvy1 <aurelien.tauvy@etu.unilim.fr>
2017-11-06 12:05:44 +0100

    tests rouges

--- src/main/java/fr/unilim/iut/katatennis/TennisGame1.java
+++ src/main/java/fr/unilim/iut/katatennis/TennisGame1.java
@@ -6,6 +6,7 @@ public class TennisGame1 implements TennisGame {
 	private int scorePlayer2 = 0;
 	private String player1Name;
 	private String player2Name;
+	private static String[] possibleScores = { "Love","Fifteen","Thirty","Forty"};
 
 	public TennisGame1(String player1Name, String player2Name) {
 		this.player1Name = player1Name;
@@ -14,64 +15,66 @@ public class TennisGame1 implements TennisGame {
 
 	public void wonPoint(String playerName) {
 		if (playerName == this.player1Name)
-			scorePlayer1 += 1;
+			scorePlayer1++;
 		else
-			scorePlayer2 += 1;
+			scorePlayer2++;
 	}
 
 	public String getScore() {
 		String score = "";
-		int tempScore = 0;
 		if (scorePlayer1 == scorePlayer2) {
-			switch (scorePlayer1) {
-			case 0:
-				score = "Love-All";
-				break;
-			case 1:
-				score = "Fifteen-All";
-				break;
-			case 2:
-				score = "Thirty-All";
-				break;
-			default:
-				score = "Deuce";
-				break;
-
-			}
+			score = getScoreEquality();
 		} else if (scorePlayer1 >= 4 || scorePlayer2 >= 4) {
-			int minusResult = scorePlayer1 - scorePlayer2;
-			if (minusResult == 1)
-				score = "Advantage player1";
-			else if (minusResult == -1)
-				score = "Advantage player2";
-			else if (minusResult >= 2)
-				score = "Win for player1";
-			else
-				score = "Win for player2";
+			score = getScoreEndGame();
 		} else {
-			for (int i = 1; i < 3; i++) {
-				if (i == 1)
-					tempScore = scorePlayer1;
-				else {
-					score += "-";
-					tempScore = scorePlayer2;
-				}
-				switch (tempScore) {
-				case 0:
-					score += "Love";
-					break;
-				case 1:
-					score += "Fifteen";
-					break;
-				case 2:
-					score += "Thirty";
-					break;
-				case 3:
-					score += "Forty";
-					break;
-				}
-			}
+			score = getScoreStandard();
 		}
+		return score;
+	}
+
+	private String getScoreStandard() {
+		String score;
+		score = possibleScores[scorePlayer1] + "-" + possibleScores[scorePlayer2];
+		return score;
+	}
+
+	private String getScoreEndGame() {
+		String score;
+		int minusResult = scorePlayer1 - scorePlayer2;
+		if (minusResult == 1)
+			score = "Advantage" + player1Name;
+		else if (minusResult == -1)
+			score = "Advantage" + player2Name;
+		else if (minusResult >= 2)
+			score = "Win for" + player1Name;
+		else
+			score = "Win for" + player2Name;
+		return score;
+	}
+
+	private String getScoreEquality() {
+		String score;
+		if(scorePlayer1<=2) {
+		score = possibleScores[scorePlayer1]+"-All";
+		}
+		else {
+			score = "Deuce";
+		}
+		/*switch (scorePlayer1) {
+		case 0:
+			score = "Love-All";
+			break;
+		case 1:
+			score = "Fifteen-All";
+			break;
+		case 2:
+			score = "Thirty-All";
+			break;
+		default:
+			score = "Deuce";
+			break;
+
+		}*/
 		return score;
 	}
 }
